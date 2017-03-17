df999ecd34403c8130aec618c865a5af8764ba7c
Robin <robinvd95@live.nl>
2017-03-17 14:36:41 +0100

    Display Center Method

--- src/rendering/Display.java
+++ src/rendering/Display.java
@@ -1,5 +1,19 @@
 package rendering;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
 public class Display {
 	
+	
+	public void centerFrame(JFrame frame, int width, int height){
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+		int monitorWidth = gd.getDisplayMode().getWidth();
+		int monitorHeight = gd.getDisplayMode().getHeight();
+
+		frame.setBounds((monitorWidth - width) / 2, (monitorHeight - height) / 2, width, height);
+	}
 }
