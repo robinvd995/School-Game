204055aefb0f15b59695f52559017663b124dbce
Robin <robinvd95@live.nl>
2017-03-17 13:46:49 +0100

    Entity Functions

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -5,6 +5,23 @@ public class Entity {
 	private int posX;
 	private int posY;
 	
+	public Entity(){
+		this(0, 0);
+	}
+	
+	public Entity(int posX, int posY){
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	public void setPosX(int newX){
+		this.posX = newX;
+	}
+	
+	public void setPosY(int newY){
+		this.posY = newY;
+	}
+	
 	public int getPosX(){
 		return posX;
 	}
