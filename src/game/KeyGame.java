bcfeba5cbef942364b89305eea96e1d8cfbee0fa
Xu <xwjl92@hotmail.com>
2017-03-20 15:14:25 +0100

    a

--- src/game/KeyGame.java
+++ src/game/KeyGame.java
@@ -1,17 +1,24 @@
 package game;
 
-import entity.Player;
 import input.InputManager;
 import rendering.Display;
 
 public class KeyGame implements Runnable{
 	
 	private Display display;
+	public String title;
+	public int width, height;
+	
+	private boolean playing = false;
+	private Thread thread;
 	//private static Movement movement;
 	
 	private InputManager inputManager;
 	
 	public KeyGame(String title, int width, int height){
+		this.title = title;
+		this.width = width;
+		this.height = height;
 		display = new Display(title, width, height);
 		
 		//movement = new Movement();		
@@ -22,12 +29,48 @@ public class KeyGame implements Runnable{
 	@Override
 	public void run() {
 		init();
+		
+		while(playing){
+			tick();
+			render();
+		}
+		
+		stop();
+	}
+	
+	private void tick(){
+
+	}
+	
+	private void render(){
+		
 	}
 	
 	public void init(){
 		display.createDisplay();
 		inputManager.initKeys();
 		display.addKeyListener(inputManager);
+	}
+	
+	public synchronized void start(){
+		if(playing){
+			return;
+		}
+		playing = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		if(!playing){
+			return;
+		}
+		playing = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/*public static Movement getMovement() {
